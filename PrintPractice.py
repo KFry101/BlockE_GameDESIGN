f2ec3b7efa8174a95900f839b78bdaa40b95ae1c
KFry101 <katiefrymire@gmail.com>
2022-01-20 22:12:44 -0600

    1/20/22 Homework

---
+++ PrintPractice.py
@@ -0,0 +1,35 @@
+#Katie Frymire
+#1/19/22
+
+import os
+os.system('cls')
+
+#Print Practice Homeork
+
+#1.
+
+#Varibles
+GH=10
+PT=5.33
+Total=240
+Bys=120
+
+print('#1\n') 
+
+print("Greenhill :", GH,',', "Portal :", PT)
+print("Total Student :", Total,',', "Boys :", Bys)
+print("031")
+print('3.561E+02\n')
+
+#2
+
+print('#2\n')
+
+print("Welcome to", end=" ")
+print("Greenhill School\n")
+
+#3
+print('#3\n')
+
+string= "I\nlove\t\Greenhill"
+print("I\nLove\t\"Greenhill\"\nThe string after using repr() is :\n\'I\\nLove\\t\\Greenhill\'")
