29b1db3abae23f1658e57446b579d8933a1eabdc
KFry101 <katiefrymire@gmail.com>
2022-01-23 12:59:00 -0600

    1/23/22

---
+++ gameMenu.py
@@ -0,0 +1,21 @@
+#Katie Frymire
+#1/23/22
+
+import os 
+os.system('cls')
+
+import random
+
+#Make a menu 
+print("###################################")
+print("#     Guess a number Menu         #")
+print("###################################\n")
+# choices  1-10
+# choices 1 -50
+# choices 1-100
+#Select your choice
+
+print("#     Select your Choice     #")
+input(" A:1-10     B:1-50    C:1-100:  ")
+
+
