7bb25c53b533403828fe80cb6701b6dc9e6465f0
KFry101 <katiefrymire@gmail.com>
2022-01-31 10:10:27 -0600

    1/31/22

---
+++ stringstuff.py
@@ -0,0 +1,21 @@
+#Katie Frymire
+#1/31/22
+#strings array of characters
+#many functions
+
+import os
+os.system ('cls')
+
+MyName= "Katie Frymire"
+Mystatment= """my name is no nice blah
+
+ blah 
+ abal"""
+
+print(MyName[6])
+print( 'my last name stars with '+ MyName[6] )
+
+print(Mystatment)
+if 'blah' in Mystatment:
+    print('true')
+print('expert' not in Mystatment)
