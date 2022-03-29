b744da8c7dc3918a8c5896945f3d7b4ea6724bcd
KFry101 <katiefrymire@gmail.com>
2022-03-29 17:13:41 -0500

    3/29/22

--- Class Stuff/LearningFiles.py
+++ Class Stuff/LearningFiles.py
@@ -6,13 +6,13 @@
 #D) read 'r'
 #E) close() 
 
-import pygame, os, datetime
+import pygame as p, os, datetime
 os.system('cls')
 date=datetime.datetime.now()
 score=123
 name='Jesse'
 print(date.strftime('%m/%d/%Y'))
-scoreLine=str(score)+" "+name+" "+date.strftime('%m/%d/%Y')
+scoreLine=str(score)+"\t"+name+"\t"+date.strftime('%m/%d/%Y'+'\n')
 print (scoreLine)
 #open file and write in it
 myFile=open('Class Stuff\score.txt', 'w')
@@ -22,8 +22,15 @@ myFile.close()
 score=456
 name='Jay'
 print(date.strftime('%m/%d/%Y'))
-scoreLine=str(score)+" "+name+" "+date.strftime('%m/%d/%Y')
+scoreLine=str(score)+"\t "+name+"\t"+date.strftime('%m/%d/%Y'+'\n')
 myFile=open('Class Stuff\score.txt', 'a')
 myFile.write(scoreLine)
 
 myFile.close()
+myFile=open('Class Stuff\score.txt', 'r')
+lines= myFile.readline()
+print (lines)
+lines= myFile.readline()
+print (lines)
+myFile.close()
+
