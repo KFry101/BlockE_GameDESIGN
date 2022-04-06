0aee483a47e126b7c04111661308207c7e5dc04d
KFry101 <katiefrymire@gmail.com>
2022-04-05 22:24:23 -0500

    4/5/22

--- Class Stuff/CircleEatsSquare/movingCharaacter.py
+++ Class Stuff/CircleEatsSquare/movingCharaacter.py
@@ -1,6 +1,7 @@
-#Katie
+#Katie Frymire
+#4/5/22
 
-from email.mime import image
+
 import os, random, math,datetime
 import pygame as p
 os.system('cls')
@@ -8,37 +9,86 @@ os.system('cls')
 #intailize p
 p.init()
 
-#MENU VARIABLES
+#Constants
+JUMP=False
+MAX=10
 WIDTH=700
 HEIGHT=700
+
 #screen
 screen=p.display.set_mode((WIDTH,HEIGHT))
 p.display.set_caption("Final Project")
 
-bg=p.image.load('Class Stuff\images\\bi mountain.jpg')
-character=p.image.load("Class Stuff\images\Pygame-Tutorials-master\Game\standing.png")
-screen.blit(bg,(0,0))
-screen.blit(character,(300,300))
-p.display.update()
-p.time.delay(300)
-x=300
-y=300
+#characters and images
+walkRight = [p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R1.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R2.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R3.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R4.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R5.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R6.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R7.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R8.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\R9.png')]
+walkLeft = [p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L1.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L2.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L3.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L4.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L5.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L6.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L7.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L8.png'), p.image.load('Class Stuff\images\Pygame-Tutorials-master\Game\L9.png')]
+biM=p.image.load('Class Stuff\images\\bi mountain.jpg')
+brW=p.image.load('Class Stuff\images\\broke window.jpg')
+chara=p.image.load("Class Stuff\images\Pygame-Tutorials-master\Game\standing.png")
+
+#clock
+clock = p.time.Clock()
+
+x=30
+y=636
+wc=64
+hc=64
 move= 5
 check=True
+jumpCount=10
+left= False
+right=False
+walkCount=0
+
+def drawWindow():
+    global walkCount
+    screen.blit(biM,(0,0))
+    
+    if walkCount + 1 >= 27:
+         walkCount = 0
+
+    if left:
+        screen.blit(walkLeft[walkCount//3], (x,y))
+        walkCount += 1
+    elif right:
+        screen.blit(walkRight[walkCount//3], (x,y))
+        walkCount +=1
+    else:
+        screen.blit(chara, (x,y))
+    
+    p.display.update()
+
+
+#main loop
 while check:
-    screen.blit(bg,(0,0))
+    clock.tick(27)
     for event in p.event.get():
         if event.type == p.QUIT:
             check = False
     keys=p.key.get_pressed()
-    if keys[p.K_LEFT]:
+    if keys[p.K_LEFT] and x >=-14:
         x -= move
-    if keys[p.K_RIGHT]:
+        left=True
+        right=False
+    elif keys[p.K_RIGHT] and x<= WIDTH-50:
         x += move
-    if keys[p.K_UP]:
-        y-= move
-    if keys[p.K_DOWN]:
-        y+= move
-    screen.blit(character,(x,y))
-    p.display.update()
-    p.time.delay(10)
+        right=True
+        left=False
+    else:
+        left=False
+        right=False
+        walkCount=0
+    if not JUMP:
+        if keys[p.K_SPACE]:
+            JUMP=True
+    else:
+        if jumpCount>=-MAX:
+            y -= jumpCount*abs(jumpCount)/2
+            jumpCount-=1
+        else:
+            jumpCount=MAX
+            JUMP=False
+
+    drawWindow()
+
+p.quit()
