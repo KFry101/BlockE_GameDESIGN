05fa260565f8341330869dc8eecdbd10f6752686
KFry101 <katiefrymire@gmail.com>
2022-09-26 08:21:31 -0500

    hi

--- nameSearch.py
+++ nameSearch.py
@@ -1,17 +1,21 @@
 #Katie Frymire
 #user searchs name and give numbers for years
-
+import os
+os.system('cls')
+nameLib={}
+Name= input("Enter a name: ")
 import csv
 with open('Names.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['first_name'], row['last_name'])
-
+        nameLib[row["NAME"]]={row["YEAR"]: row["NUMBER"]}
+        if row["NAME"] in nameLib:
+            nameLib.append({row["YEAR"]: row["NUMBER"]})
 # Eric Idle
 # John Cleese
 
-print(row)
-{'first_name': 'John', 'last_name': 'Cleese'}
+try:
+    print(nameLib[Name])
+except:
+    print("name is not valid for search")
 
-
-Name= input("Enter a name: ")
