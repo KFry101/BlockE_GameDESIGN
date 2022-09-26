a4f5426d86f0fca2d830f7342d968b4ff4c7e914
KFry101 <katiefrymire@gmail.com>
2022-09-26 09:00:29 -0500

    s

--- nameSearch.py
+++ nameSearch.py
@@ -2,20 +2,34 @@
 #user searchs name and give numbers for years
 import os
 os.system('cls')
+
 nameLib={}
-Name= input("Enter a name: ")
+years=[2016, 2017, 2018, 2019, 2020, 2021]
+Name= input("Enter a name (Case-Sensitive): ")
+key=Name
 import csv
 with open('Names.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        nameLib[row["NAME"]]={row["YEAR"]: row["NUMBER"]}
-        if row["NAME"] in nameLib:
-            nameLib.append({row["YEAR"]: row["NUMBER"]})
-# Eric Idle
-# John Cleese
+    
+    #Old Code
+    # for row in reader:
+    #     nameLib[row["NAME"]]={row["YEAR"]: row["NUMBER"]}
+    #     if row["NAME"] in nameLib:
+    #        
 
+#New Code
+if not (Name in nameLib):
+    nameLib[key]={}
+
+if not (years in nameLib):
+    nameLib[key][years]=10
+else: 
+    nameLib[key][years]= nameLib[key][years]+10
+
+    
 try:
-    print(nameLib[Name])
+    print(nameLib[key])
+    print(nameLib[key][years])
 except:
-    print("name is not valid for search")
+    print("Name is not valid for search.")
 
