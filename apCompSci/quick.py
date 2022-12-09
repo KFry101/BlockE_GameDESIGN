551e5fd2abd3ed19cd4f3cd5a72f91c6c926775c
KFry101 <katiefrymire@gmail.com>
2022-12-08 22:54:38 -0600

    hi

---
+++ apCompSci/quick.py
@@ -0,0 +1,15 @@
+import os
+os.system ('cls')
+
+check=True
+def whoInClass():
+    name=(input("Whos in your class? "))
+    names=[]
+    if (len(name)>3):
+        names.append(name)
+    else:
+        print("invalid name")
+
+
+while check:
+    whoInClass()
