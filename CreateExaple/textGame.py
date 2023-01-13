d115874aff3e08046d0867bd11d909f5173d385f
KFry101 <katiefrymire@gmail.com>
2023-01-13 08:34:45 -0600

    kh

--- CreateExaple/textGame.py
+++ CreateExaple/textGame.py
@@ -1,12 +1,12 @@
 import os, random
 os.system('cls')
-list = ["\nAre we friends? ", "\nCan you help me? ", "\nCan we talk? "]
-def Convo():
+list = ["\nAre we friends? ", "\nCan you help me? ", "\nCan we talk? ", ]
+def Convo(Name):
     print(list[random.randint(0,2)])
     response= input("your response:  (Y/N)")
     while (len(response)> 0):
         if  response=='y' or response== 'Y':
-            print("thanks, " + name)
+            print("thanks, " + Name)
         else:
             print("I hate my life")
         print(list[random.randint(0,2)])
@@ -18,5 +18,5 @@ def Convo():
 #Main Code 
 print("Welcome to the Friendship Simulator")
 name= input("What is your name? ")
-Convo()
+Convo(name)
 
