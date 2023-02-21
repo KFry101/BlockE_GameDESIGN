885004645792f26eb410033960754ef698211423
KFry101 <katiefrymire@gmail.com>
2023-02-21 09:20:46 -0600

    h

--- CreateExaple/createTask.py
+++ CreateExaple/createTask.py
@@ -1,24 +1,22 @@
-import os
-import random 
+#imports library for random
+import random
 
-#next line clears terminal each time program is ran
-os.system('cls') 
-
-#This is the list of character that the random generation/method with access
+#This is the list of character that the random generation/method will access
 characters= ["a","b","c","d","e","f","g","h","i","j","k","l","m",'n',"o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F",'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 '0','1','2','3','4','5','6','7','8','9','!','"','#','$','%','&','*','+',':','?','@']
 
 #define the method and the parameter "leng"
 def getPassword(leng):
-
-    #defines pssword variable
+    #declare password variable
     password = ""
-
-    
+    #branching path for test case 1
     if int(leng)<6:
-        print("This password is will not be safe. Please try again.")
+        print("This password is may not be secure. Please something longer.")
+   #branching path for test case 2
     else:
+        #a loop thorugh 0 to paramenter leng
         for i in range(int(leng)):
+         #redeclares the passsword variable to a randomly selected elements from the characters list
          password= password + random.choice(characters) #this line of code is modified from https://pynative.com/python-generate-random-string/ 
         print(password)
 
